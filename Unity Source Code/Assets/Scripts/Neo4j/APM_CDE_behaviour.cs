dd77c0e32701aa6d3a779623555a659a9e4162d5
Sohail el Sabrouty <sohail.elsabrouty202@gmail.com>
2023-04-02 04:29:39 +0200

    Fixed Lerping (still ugly tho)
    Fixed the lerping by resetting the rotation variable defined in the PlayerController script but it looks like it can be cleaned up more

--- Unity Source Code/Assets/Scripts/Neo4j/APM_CDE_behaviour.cs
+++ Unity Source Code/Assets/Scripts/Neo4j/APM_CDE_behaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -13,10 +14,22 @@ namespace GraphFoundation
         public int nodeID;
         public List<string> labels = new List<string>();
         public Dictionary<string,object> properties = new Dictionary<string,object>();
-        LineRenderer drawLine;
+        public Camera CameraToMove;
+        public float MovementSpeed = 0.5F;
+        private LineRenderer drawLine;
+        private Vector3 NormalVector;
+        private Vector3 CameraStartPosition;
+        private Vector3 CameraEndPosition;
+        private Quaternion CameraStartRotation;
+        private Quaternion CameraEndRotation;
+        private bool DoMovement = false;
+        private float LerpMovement = 0;
+        float rotationThreshold = 0.01f; // example rotation threshold
+
 
         void Start()
         {
+            CameraToMove = Camera.main;
             foreach (var kvp in properties)
             {
                 if (kvp.Key == "rdfs__label")
@@ -35,6 +48,14 @@ namespace GraphFoundation
 
         private void Update()
         {
+            checkIfIntersectsWithCamera();
+            MoveToPointUpdate();
+            // have text always face camera
+            transform.GetComponentInChildren<TextMeshProUGUI>().transform.RotateAround(transform.position, Vector3.up, 20 * Time.deltaTime);
+        }
+
+        private void checkIfIntersectsWithCamera()
+        {
             // Cast a ray from the main camera
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
 
@@ -49,20 +70,38 @@ namespace GraphFoundation
             {
                 // If the ray intersects with an object, change its color to red
                 Renderer renderer = hit.collider.GetComponent<Renderer>();
-                if (renderer != null)
+                // Change the color of the object to yellow
+                renderer.material.color = Color.yellow;
+                if (Input.GetMouseButtonDown(0))
                 {
-                    // Change the color of the object to red
-                    renderer.material.color = Color.yellow;
+                    CameraStartPosition = CameraToMove.transform.position;
+                    CameraStartRotation = CameraToMove.transform.rotation;
+                    CameraEndPosition = new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z -50);
+                    CameraEndRotation = Quaternion.identity;
+                    LerpMovement = 0F;
+                    DoMovement = true;
                 }
             }
             else
             {
                 gameObject.GetComponent<Renderer>().material.color = Color.green;
             }
-
-
-            // have text always face camera
-            transform.GetComponentInChildren<TextMeshProUGUI>().transform.RotateAround(transform.position, Vector3.up, 20 * Time.deltaTime);
+        }
+        private void MoveToPointUpdate()
+        {
+            if (DoMovement)
+            {
+                CameraToMove.transform.position = Vector3.Lerp(CameraStartPosition, CameraEndPosition, LerpMovement);
+                CameraToMove.transform.rotation = Quaternion.Lerp(CameraStartRotation, CameraEndRotation, LerpMovement);
+                LerpMovement += Time.deltaTime * MovementSpeed;
+                if (LerpMovement >= 1F)
+                {
+                    LerpMovement = 1F;
+                    DoMovement = false;
+                    CameraToMove.transform.GetComponent<PlayerController>().rotation.x = 0;
+                    CameraToMove.transform.GetComponent<PlayerController>().rotation.y = 0;
+                }
+            }
         }
     }
 }
--- Unity Source Code/Assets/Scripts/PlayerController.cs
+++ Unity Source Code/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,8 @@ public class PlayerController : MonoBehaviour
         get { return sensitivity; }
         set { sensitivity = value; }
     }
-
+    public Quaternion xQuat;
+    public Quaternion yQuat;
     private float speed = 10.0f;
     private float frontalMovementInput;
     private float sidewayMovementInput;
@@ -22,7 +23,7 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Limits vertical camera rotation. Prevents the flipping that happens when rotation goes above 90.")]
     [Range(0f, 90f)][SerializeField] float yRotationLimit = 88f;
 
-    Vector2 rotation = Vector2.zero;
+    public Vector2 rotation = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -50,9 +51,10 @@ public class PlayerController : MonoBehaviour
         // Camera movement
         rotation.x += Input.GetAxis("Mouse X") * sensitivity;
         rotation.y += Input.GetAxis("Mouse Y") * sensitivity;
+       
         rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
-        var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
-        var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+        xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+        yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
 
         transform.localRotation = xQuat * yQuat;
         // Lock cursor in window and disable presence
